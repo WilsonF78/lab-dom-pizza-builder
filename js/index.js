6900a2a6b6d10a40d0ac9b9129ad0bb9fdd9c138
WilsonF78 <fortes.wilson78@gmail.com>
2022-05-21 12:57:43 +0200

    Iteration 3 ok

--- js/index.js
+++ js/index.js
@@ -92,10 +92,40 @@ function renderGlutenFreeCrust() {
   }
 }
 
+// Iteration 3: add/remove the class "active" of each `<button class="btn">`
 function renderButtons() {
-  // Iteration 3: add/remove the class "active" of each `<button class="btn">`
-}
+  if (state.pepperoni){
+    document.querySelector('.btn.btn-pepperoni').classList.add('active')
+  }
+  else {
+    document.querySelector('.btn.btn-pepperoni').classList.remove('active')
+  }
+  if (state.greenPeppers){
+    document.querySelector('.btn.btn-green-peppers').classList.add('active')
+  }
+  else {
+    document.querySelector('.btn.btn-green-peppers').classList.remove('active')
+  }
+  if (state.whiteSauce){
+    document.querySelector('.btn.btn-sauce').classList.add('active')
+  }
+  else {
+    document.querySelector('.btn.btn-sauce').classList.remove('active')
+  }
+  if (state.glutenFreeCrust){
+    document.querySelector('.btn.btn-crust').classList.add('active')
+  }
+  else {
+    document.querySelector('.btn.btn-crust').classList.remove('active')
+  }
+  if (state.mushrooms){
+    document.querySelector('.btn.btn-mushrooms').classList.add('active')
+  }
+  else {
+    document.querySelector('.btn.btn-mushrooms').classList.remove('active')
+  }
 
+ }
 function renderPrice() {
   // Iteration 4: change the HTML of `<aside class="panel price">`
 }
